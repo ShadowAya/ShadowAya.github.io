ade2e41e111422ad87090d4a58a9617a9f86a1e3
ShadowAya <jonhlad@gmail.com>
2022-12-25 15:38:01 +0100

    1.1 fix

---
+++ Media/timetable/downloads/timetable-script-static.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta
+import sys, json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--day", type=str, required=True)
+args = parser.parse_args()
+
+
+if args.day == "today" :
+    day = datetime.today()
+elif args.day == "tmr" or args.day == "tomorrow" :
+    day = datetime.today() + timedelta(days = 1)
+else :
+    raise Exception("Invalid day specified")
+
+day = day.weekday()
+
+###########################################################################################################
+
+#  🅴🅳🅸🆃 🅷🅴🆁🅴
+
+MON = [
+    {
+        "name" : "ENG",
+        "classroom" : "05",
+        "start" : "9:00",
+        "end" : "9:45"
+    },{
+        "name" : "MAT",
+        "classroom" : "15",
+        "start" : "10:00",
+        "end" : "10:45"
+    }
+]
+TUE = []
+WED = []
+THU = []
+FRI = []
+SAT = "Weekend" # to mark as off-day, replace "[]" with a reason
+SUN = "Weekend" #
+
+###########################################################################################################
+
+data = [MON, TUE, WED, THU, FRI, SAT, SUN][day]
+
+if len(data) == 0 :
+    no_school = "No School"
+    data = {}
+elif type(data) == "list" :
+    no_school = "false"
+    newdata = {}
+    for i in range(len(data)) :
+        newdata[f'{i}'] = data[i]
+    data = newdata
+
+else  :
+    no_school = data
+    data = {}
+
+
+print({
+    "no_school" : no_school,
+    "data" : data
+})
